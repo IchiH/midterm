5bb4bc5948f97b05c031524ce912f9cc28f7bab2
IchiH <msss3776@gmail.com>
2015-11-26 12:28:44 +0900

    テスト２

--- js/07.js
+++ js/07.js
@@ -1,6 +1,13 @@
 function distance(a,b,c,d){
   if(a == c && b == d){
     return 0;
-  }
-  return 0;
+  }if(a != c && b != d) {
+　　var e, f, g;
+
+　　e = a - c;
+　　f = b - d;
+　　g = Math.sqrt(Math.pow(e,2) + Math.pow(f,2));
+
+　　return g;
+}
 }
